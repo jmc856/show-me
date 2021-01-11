7a4cd4b7aec45bd79d5e701a170f5dedb274879a
Jeffrey Calvert <jeffc@stratisiot.com>
2021-01-11 04:05:05 +0000

    Remove running keepAlive script

--- keepAlive.sh
+++ keepAlive.sh
@@ -1,5 +1,6 @@
 # Bash script to ping app every 300 seconds.
 # Created because Heroku free servers go to sleep after 1 hour
+# To run this, update package.json npm start script to include & bash keepAlive.sh
 HOST="https://show-me0.herokuapp.com"
 
 while true
